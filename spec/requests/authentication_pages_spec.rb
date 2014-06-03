a32d0b601a83efcd447560ad0900296c8bcb482c
Lee Poohachoff <l.poohachoff@ivrnet.com>
2014-06-03 16:50:38 -0600

    create schema for microposts, add showing microposts to user profile

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -51,6 +51,20 @@ describe "Authentication" do
     describe "for non-signed-in users" do
       let(:user) {FG.create(:user) }
 
+
+      describe "in the Microposts controller" do
+
+        describe "submitting to the create action" do
+          before { post microposts_path }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+
+        describe "submitting to the destroy action" do
+          before { delete micropost_path(FG.create(:micropost)) }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+      end
+
       describe "when attempting to visit a protected page" do
         before do
           visit edit_user_path(user)
