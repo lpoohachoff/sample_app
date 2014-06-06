7b96f09766a5ea65623aca4a4ad37f3dc3038c0b
Lee Poohachoff <l.poohachoff@ivrnet.com>
2014-06-06 13:42:20 -0600

    install slim and simple_form. Create ability to add micropost

--- Gemfile
+++ Gemfile
@@ -43,6 +43,8 @@ gem 'sdoc', '~> 0.4.0',          group: :doc
 # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
 gem 'spring',        group: :development
 
+gem "slim-rails"
+gem 'simple_form'
 
 group :production do
   gem 'pg', '~> 0.17.1'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140602150127) do
+ActiveRecord::Schema.define(version: 20140604160959) do
+
+  create_table "foos", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "microposts", force: true do |t|
     t.string   "content"
